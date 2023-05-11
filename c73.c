f517e59ababbb31f28676173f05f7e2df93e3812
dltmqtjd <133184109+dltmqtjd@users.noreply.github.com>
2023-05-11 12:09:00 +0900

    c언어

---
+++ c73.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int num = 256;
+    char grade = 'A';
+    double pi = 3.141592;
+    printf("10진수 %d, 8진수 %o, 16진수%x%X\n", num,8,num-1,10+5);
+    printf("문자열: %s \n", "Hello World");
+    printf("pi: %f, 100의 지수표기법 %e, %E\n", pi, 100.0,100.);
+    printf("퍼센트 출력 %% 따옴표 출력 \"\n");
+    return 0;
+}
